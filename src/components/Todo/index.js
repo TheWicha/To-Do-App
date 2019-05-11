4a6bb74f1c697074c3fc4278f396991183ff2c49
TheWicha <kontakt.danielwisniewski@gmail.com>
2019-05-11 13:41:27 +0200

    change done icon position and style

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 export default (props) => (
   <li>
-    <i onClick={props.toggleComplete} style={{ color: props.item.complete ? '#5CBF2A' : '' }} className="fas fa-check"></i>
     <span onClick={props.toggleComplete} style={{ textDecoration: props.item.complete ? 'line-through' : '' }} >
       {props.item.inputValue}
     </span>
-    <i className="fas fa-trash-alt" onClick={props.onDelete}></i>
+    <div className='todo-icons'>
+      <i onClick={props.toggleComplete} style={{ color: props.item.complete ? '#5CBF2A' : '' }} className="fas fa-check"></i>
+      <i className="fas fa-trash-alt" onClick={props.onDelete}></i>
+    </div>
   </li>
 )
